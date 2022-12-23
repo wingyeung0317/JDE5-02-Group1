f35c8463b3b19d8e41830066fcb83b2013bedab1
JDE - Jo Lee <47248934+jojo11242@users.noreply.github.com>
2022-12-23 14:30:26 +0800

    Update task.py
    完成TASK3

--- task.py
+++ task.py
@@ -12,12 +12,17 @@ def hammer_pickOneMemeber():
     result = random.sample(teamJDE, 1)
     return result
 
+def taskThree():
+    text = './news.txt'
+    f = open(text, "r")
+    return(f.read()[::-1])
+
 
 if __name__ == "__main__":
     print(hammer_pickOneMemeber())
     # print(taskOne())
     # print(taskTwo())
-    # print(taskThree())
+    print(taskThree())
     # print(taskFour())
 
     # Task-1 - count the total number of words in the prargraph that contains vowel characters(a, e, i, o u)
@@ -25,5 +30,5 @@ if __name__ == "__main__":
     # Task-2 - encode the paragraph by shifting the position of each character by a variable value e.g. I am a boy (1) -> J bn b cpz
 
     # Task-3 - Reverse the entire paragraph line by line e.g. I am a boy -> yob a ma I
-
+    
     # Task-4 - Reverse the order of character of each word e.g. I am a boy -> I ma a yob
